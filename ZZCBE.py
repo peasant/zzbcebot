d5a45a536b611239f4d434649bd7a0a8bd4a7710
Shan <shanjiang.g@gmail.com>
2016-06-17 07:38:52 +0800

    连接服务器、取得商品信息。

--- ZZCBE.py
+++ ZZCBE.py
@@ -5,101 +5,106 @@ import ctypes
 from ctypes import *
 import time
 
-'''
-模拟账号：018888027
-密码： trader
-'''
-
-
-
 
 import win32com.client
 from win32com.client import gencache
 
-ZZBCE_ServerIp='122.226.14.236'
-ZZBCE_ServerPort=58095 #52768 #
-
-'''非模拟账号'''
-TRADER_ID="010500001"
-TRADER_PSWD="trade123"
+ZZBCE_ServerIp = '122.226.14.236'
+#ZZBCE_ServerPort = 52000 #57000 #57000为模拟端口 #57268 # 48095#
+#
+# '''非模拟账号'''
+# TRADER_ID = "010500001"
+# TRADER_PSWD = "trader123"
+# ZZBCE_ServerPort= 52000
 
 '''模拟账号'''
-# TRADER_ID="018888027"
-# TRADER_PSWD="trader"
-CONNECTSTATUS= 0
-data=''
-errorId=5566
-ret=1 #失败
+TRADER_ID = "018888027"
+TRADER_PSWD ="trader"
+ZZBCE_ServerPort= 57000
 
-class callBackEvents(object):
+connectStatus = 0
+data = ''
+errorId = 5566
+ret = 1  # 失败
+
+
+# eventobj=win32com.client.getevents("TradeCOM.Trade")
+class callBackEvents():
     """ Callback Object for win32com
     """
+    #TODO 传递xmldata出去
+    def __init__(self,xmldata=''):
+        xmldata=''
+
+    def get_params(self,xmldata):
+        return self.xmldata
 
     def OnConnected(self):
         """on connected"""
-        print('Connected.\n')
+        print('已连接.')
 
     def OnDisconnected(self):
         """on disconnected"""
-        print('Disconnected\n')
+        print('已断开')
 
-    def OnRecvData(self, xmldata):
+    def OnRecvData(self,data ):
         """receive data"""
-        print("receive:{}".format(xmldata))
+        self.xmldata=data
+        print("RECV :{}".format(data))
 
-callBackEvents.xmldata=data
+# market = win32com.client.Dispatch("TradeCOM.Market")
+# marketEvents = win32com.client.WithEvents(market, callBackEvents)
+# ret = market.Login(ZZBCE_ServerIp, ZZBCE_ServerPort, TRADER_ID, TRADER_PSWD, 300)
 
-tradeEvents= win32com.client.DispatchWithEvents("TradeCOM.Trade",callBackEvents)
-#print(win32com.client.getevents("TradeCOM.Trade"))
-
-#trade = gencache.EnsureDispatch('TradeCOM.Trade')
-market = win32com.client.Dispatch("TradeCOM.Market")
 trade = win32com.client.Dispatch("TradeCOM.Trade")
+#tradeEvents = win32com.client.WithEvents(trade, callBackEvents)
+trade= win32com.client.DispatchWithEvents("TradeCOM.Trade", callBackEvents)
+ret = trade.Login(ZZBCE_ServerIp, ZZBCE_ServerPort, TRADER_ID, TRADER_PSWD, 300,)
 
+connectStatus = trade.Connected
+time.sleep(0.2)
+print(trade.get_params(data))
 
-#ret = market.Login('122.226.14.236', 52768, '010500001', 'trader123', 60)
-#ret = market.Login(ZZBCE_ServerIp, ZZBCE_ServerPort, TRADER_ID, TRADER_PSWD, 300)
-ret = trade.Login(ZZBCE_ServerIp, ZZBCE_ServerPort, TRADER_ID, TRADER_PSWD,50)
-#time.sleep(5)
-
-CONNECTSTATUS= trade.Connected
-
-
-if CONNECTSTATUS:
-    print('服务器已连接')
-else:
-    print('服务器未连接')
-
-data='<Send ID="1001" />'
-
-if trade.Execute(data) == 0:  #指令发送成功
-    print(tradeEvents.OnRecvData(data))
-else:
-    print('\nlasterror:', trade.LastError)
-
-#print('ErrorID:', errorId)
-
-
-
-#
-# from tkinter import *
-# import time
-#
-#
-# app=Tk()
-# # ret = trade.Login(ZZBCE_ServerIp, ZZBCE_ServerPort, '018888027', 'trader', 60)
-# if CONNECTSTATUS==True:
-#     status= '已连接'
+# if trade.Connected == True:
+#     print('服务器已连接')
 # else:
-#     status= '未连接'
-#
-# #time.sleep(2)
-# connectStatus=Label(app, text=status)
-# connectStatus.pack()
-#
-#
-#
-# #app.master.title('ZZBCE辅助程序')
-# app.mainloop()
+#     print('服务器未连接')
+
+data = '<Send ID="2102" WareID="ZPCA1" />'
+
+
+ret= trade.Execute(data)   # 指令发送成功
+time.sleep(0.2)
+recvData= trade.get_params(data)
+
+#TODO 解析XML
+print (recvData)
+
+if ret ==0:
+    print()
+else:
+    print('lasterror:', trade.LastError)
+
+    #
+    # from tkinter import *
+    # import time
+    #
+    #
+    # app=Tk()
+    # # ret = trade.Login(ZZBCE_ServerIp, ZZBCE_ServerPort, '018888027', 'trader', 60)
+    # if connectStatus==True:
+    #     status= '已连接'
+    # else:
+    #     status= '未连接'
+    #
+    # #time.sleep(2)
+    # cnctStatus=Label(app, text=status)
+    # cnctStatus.pack()
+    #
+    #
+    #
+    # #app.master.title('ZZBCE辅助程序')
+    # app.mainloop()
 
 trade.Logout()
+
