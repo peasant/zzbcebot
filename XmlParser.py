41e0ff9323ebe628cf3469682ed6f6ea2255529d
Shan <shanjiang.g@gmail.com>
2016-06-17 08:59:15 +0800

    新建xml解析文件

---
+++ XmlParser.py
@@ -0,0 +1,6 @@
+#xml解析文件
+
+import
+import sys
+
+xmlData= "<Recv ID="2102"><Data WareID="ZPCA1" WareName="大黄鱼A1" LimitUP="29.38" LimitDown="26.58" EnabledMoney="499393.11" OpenPrice="27.80" NewPrice="27.72" RaiseLose="-0.26" RaiseLose2="-0.9292" HighPrice="27.82" LowPrice="27.70" AvgPrice="27.76" ContNum="261634" HoldNum="139462" Yesterday="27.98" BuyPrice1="27.72" BuyPrice2="27.70" BuyPrice3="27.68" BuyPrice4="0.00" BuyPrice5="0.00" BuyQty1="183" BuyQty2="1253" BuyQty3="98" BuyQty4="0" BuyQty5="0" SalePrice1="27.74" SalePrice2="27.76" SalePrice3="27.78" SalePrice4="0.00" SalePrice5="0.00" SalQty1="708" SalQty2="96" SalQty3="50" SalQty4="0" SalQty5="0" MinUnit="1" MinChgPrice="0.02" SaleNum="261" BuyNum="35" MAXSQty="17905" MAXBQty="17905" CURSHold="261" CURBHold="35" SMaxNum="2000" DepotNum="0" CSaleNum="0" CBuyNum="0" PriceDecimal="2" /></Recv>"
--- ZZCBE.py
+++ ZZCBE.py
@@ -5,12 +5,11 @@ import ctypes
 from ctypes import *
 import time
 
-
 import win32com.client
 from win32com.client import gencache
 
 ZZBCE_ServerIp = '122.226.14.236'
-#ZZBCE_ServerPort = 52000 #57000 #57000为模拟端口 #57268 # 48095#
+# ZZBCE_ServerPort = 52000 #57000 #57000为模拟端口 #57268 # 48095#
 #
 # '''非模拟账号'''
 # TRADER_ID = "010500001"
@@ -19,8 +18,8 @@ ZZBCE_ServerIp = '122.226.14.236'
 
 '''模拟账号'''
 TRADER_ID = "018888027"
-TRADER_PSWD ="trader"
-ZZBCE_ServerPort= 57000
+TRADER_PSWD = "trader"
+ZZBCE_ServerPort = 57000
 
 connectStatus = 0
 data = ''
@@ -32,11 +31,12 @@ ret = 1  # 失败
 class callBackEvents():
     """ Callback Object for win32com
     """
-    #TODO 传递xmldata出去
-    def __init__(self,xmldata=''):
-        xmldata=''
 
-    def get_params(self,xmldata):
+    # TODO 传递xmldata出去
+    def __init__(self, xmldata=''):
+        xmldata = ''
+
+    def get_params(self, xmldata):
         return self.xmldata
 
     def OnConnected(self):
@@ -47,19 +47,20 @@ class callBackEvents():
         """on disconnected"""
         print('已断开')
 
-    def OnRecvData(self,data ):
+    def OnRecvData(self, data):
         """receive data"""
-        self.xmldata=data
+        self.xmldata = data
         print("RECV :{}".format(data))
+
 
 # market = win32com.client.Dispatch("TradeCOM.Market")
 # marketEvents = win32com.client.WithEvents(market, callBackEvents)
 # ret = market.Login(ZZBCE_ServerIp, ZZBCE_ServerPort, TRADER_ID, TRADER_PSWD, 300)
 
 trade = win32com.client.Dispatch("TradeCOM.Trade")
-#tradeEvents = win32com.client.WithEvents(trade, callBackEvents)
-trade= win32com.client.DispatchWithEvents("TradeCOM.Trade", callBackEvents)
-ret = trade.Login(ZZBCE_ServerIp, ZZBCE_ServerPort, TRADER_ID, TRADER_PSWD, 300,)
+# tradeEvents = win32com.client.WithEvents(trade, callBackEvents)
+trade = win32com.client.DispatchWithEvents("TradeCOM.Trade", callBackEvents)
+ret = trade.Login(ZZBCE_ServerIp, ZZBCE_ServerPort, TRADER_ID, TRADER_PSWD, 300, )
 
 connectStatus = trade.Connected
 time.sleep(0.2)
@@ -72,15 +73,14 @@ print(trade.get_params(data))
 
 data = '<Send ID="2102" WareID="ZPCA1" />'
 
-
-ret= trade.Execute(data)   # 指令发送成功
+ret = trade.Execute(data)  # 指令发送成功
 time.sleep(0.2)
-recvData= trade.get_params(data)
+recvData = trade.get_params(data)
 
-#TODO 解析XML
-print (recvData)
+# TODO 解析XML
+print(recvData)
 
-if ret ==0:
+if ret == 0:
     print()
 else:
     print('lasterror:', trade.LastError)
@@ -107,4 +107,3 @@ else:
     # app.mainloop()
 
 trade.Logout()
-
